f85e403d1fc73bf3d916836b1dada161dd783d9a
yuanhang <yuanhans@usc.edu>
2025-03-05 21:57:42 -0800

    create flowers17 dataset; todo: change route for eval

---
+++ yolox/data/datasets/flowers17_classes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Megvii, Inc. and its affiliates.
+
+# VOC_CLASSES = ( '__background__', # always index 0
+FLOWERS17_CLASSES = (
+    "cowslip",
+    "tulip",
+    "tigerlily",
+    "crocus",
+    "bluebell",
+    "lilyvalley",
+    "snowdrop"
+    "windflower",
+    "sunflower",
+    "pansy",
+    "iris",
+    "fritillary",
+    "dandelion",
+    "daisy",
+    "daffodil",
+    "colts'foot",
+    "buttercup",
+)
