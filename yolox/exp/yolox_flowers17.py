378f5cd8002cfc179a3bbbc5a818c0e367285e1e
Yuanhang Su <yuanhangsu@yuanhangs.nvidia.com>
2025-03-09 18:18:29 -0700

    add tensorboard results

--- yolox/exp/yolox_flowers17.py
+++ yolox/exp/yolox_flowers17.py
@@ -39,7 +39,7 @@ class Exp(BaseExp):
         # epoch number used for warmup
         self.warmup_epochs = 5
         # max training epoch
-        self.max_epoch = 300
+        self.max_epoch = 200
         # last #epoch to close augmention like mosaic
         self.no_aug_epochs = 15
 
